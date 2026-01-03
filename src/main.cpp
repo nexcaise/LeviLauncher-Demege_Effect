3fee6c0f9598f20451b527021ad6b1ae1927ac2d
MCPEOVER <118625383+MCPEOVER@users.noreply.github.com>
2026-01-03 23:30:47 +0900

    Refactor and enhance ImGui integration in main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,26 @@
 #include <jni.h>
-#include <android/input.h>
 #include <EGL/egl.h>
 #include <GLES3/gl3.h>
+#include <android/input.h>
 #include <pthread.h>
 #include <unistd.h>
+#include <stdint.h>
 
-#include "pl/Hook.h"
-#include "pl/Gloss.h"
+#include "imgui.h"
+#include "backends/imgui_impl_android.h"
+#include "backends/imgui_impl_opengl3.h"
 
-#include "ImGui/imgui.h"
-#include "ImGui/backends/imgui_impl_opengl3.h"
-#include "ImGui/backends/imgui_impl_android.h"
+#include <dlfcn.h>
 
-static bool g_initialized = false;
-static int g_width = 0, g_height = 0;
-static EGLBoolean (*orig_eglswapbuffers)(EGLDisplay, EGLSurface) = nullptr;
+static bool imgui_init = false;
+static int screen_w = 0, screen_h = 0;
+
+static EGLBoolean (*orig_eglSwapBuffers)(EGLDisplay, EGLSurface);
+
+static bool menu_open = true;
 
 static bool enchant_enabled[7];
+static int enchant_level[7];
 
 static const char* enchant_names[7] = {
     "Copper Spear",
@@ -28,146 +32,119 @@ static const char* enchant_names[7] = {
     "Wood Spear"
 };
 
-const unsigned char epd_bitmap_copper_spear[] = {
-0xff,0xf8,0xff,0xe6,0xff,0x9c,0xfe,0x79,0xfe,0xf1,0xff,0x63,0xff,0x43,0xfe,0x07,
-0xfc,0x67,0xf8,0xff,0xf1,0xff,0xe3,0xff,0xc7,0xff,0x8f,0xff,0x1f,0xff,0x3f,0xff
+static GLuint icon_texture = 0;
+
+static unsigned char icon_rgba[16 * 16 * 4] = {
+    255,255,255,0, 255,255,255,0, 255,255,255,0, 255,255,255,0,
+    255,255,255,0, 255,255,255,255,255,255,255,255,255,255,255,0,
+    255,255,255,0, 255,255,255,255,255,255,255,255,255,255,255,0,
+    255,255,255,0, 255,255,255,0, 255,255,255,0, 255,255,255,0,
 };
 
-const unsigned char epd_bitmap_diamond_spear[] = {
-0xff,0xf8,0xff,0xe6,0xff,0x9e,0xfe,0x7d,0xfe,0xf9,0xff,0x73,0xff,0x63,0xfe,0x07,
-0xfc,0x67,0xf8,0xff,0xf1,0xff,0xe3,0xff,0xc7,0xff,0x8f,0xff,0x1f,0xff,0x3f,0xff
-};
-
-const unsigned char epd_bitmap_gold_spear[] = {
-0xff,0xf8,0xff,0xe6,0xff,0x9e,0xfe,0x7d,0xfe,0xfd,0xff,0x7b,0xff,0x7b,0xfe,0x17,
-0xfc,0x67,0xf8,0xff,0xf1,0xff,0xe3,0xff,0xc7,0xff,0x8f,0xff,0x1f,0xff,0x3f,0xff
-};
-
-const unsigned char epd_bitmap_iron_spear[] = {
-0xff,0xf8,0xff,0xe6,0xff,0x9c,0xfe,0x79,0xfe,0xf1,0xff,0x63,0xff,0x43,0xfe,0x07,
-0xfc,0x67,0xf8,0xff,0xf1,0xff,0xe3,0xff,0xc7,0xff,0x8f,0xff,0x1f,0xff,0x3f,0xff
-};
-
-const unsigned char epd_bitmap_netherite_spear[] = {
-0xff,0xf8,0xff,0xe0,0xff,0x80,0xfe,0x01,0xfe,0x01,0xff,0x03,0xff,0x03,0xfe,0x07,
-0xfc,0x67,0xf8,0xff,0xf1,0xff,0xe3,0xff,0xc7,0xff,0x8f,0xff,0x1f,0xff,0x3f,0xff
-};
-
-const unsigned char epd_bitmap_stone_spear[] = {
-0xff,0xf8,0xff,0xe6,0xff,0x9c,0xfe,0x39,0xfe,0x71,0xff,0x63,0xff,0x43,0xfe,0x07,
-0xfc,0x67,0xf8,0xff,0xf1,0xff,0xe3,0xff,0xc7,0xff,0x8f,0xff,0x1f,0xff,0x3f,0xff
-};
-
-const unsigned char epd_bitmap_wood_spear[] = {
-0xff,0xf8,0xff,0xe0,0xff,0x80,0xfe,0x01,0xfe,0x01,0xff,0x03,0xff,0x03,0xfe,0x07,
-0xfc,0x67,0xf8,0xff,0xf1,0xff,0xe3,0xff,0xc7,0xff,0x8f,0xff,0x1f,0xff,0x3f,0xff
-};
-
-const unsigned char* epd_bitmap_allArray[7] = {
-    epd_bitmap_copper_spear,
-    epd_bitmap_diamond_spear,
-    epd_bitmap_gold_spear,
-    epd_bitmap_iron_spear,
-    epd_bitmap_netherite_spear,
-    epd_bitmap_stone_spear,
-    epd_bitmap_wood_spear
-};
-
-static GLuint enchant_textures[7];
-
-static GLuint make_texture(const unsigned char* bmp) {
-    unsigned char rgba[16 * 16 * 4] = {0};
-    for (int y = 0; y < 16; y++) {
-        for (int x = 0; x < 16; x++) {
-            int bi = (y * 16 + x) / 8;
-            int bt = 7 - (x % 8);
-            if ((bmp[bi] >> bt) & 1) {
-                int i = (y * 16 + x) * 4;
-                rgba[i] = rgba[i+1] = rgba[i+2] = rgba[i+3] = 255;
-            }
-        }
-    }
-    GLuint t;
-    glGenTextures(1, &t);
-    glBindTexture(GL_TEXTURE_2D, t);
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, 16, 16, 0, GL_RGBA, GL_UNSIGNED_BYTE, rgba);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-    return t;
+static void InitTexture() {
+    glGenTextures(1, &icon_texture);
+    glBindTexture(GL_TEXTURE_2D, icon_texture);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexImage2D(
+        GL_TEXTURE_2D,
+        0,
+        GL_RGBA,
+        16,
+        16,
+        0,
+        GL_RGBA,
+        GL_UNSIGNED_BYTE,
+        icon_rgba
+    );
 }
 
-static void draw_menu() {
-    ImGui::Begin("Enchant", nullptr, ImGuiWindowFlags_NoCollapse);
-    for (int i = 0; i < 7; i++) {
-        ImGui::Image((void*)(intptr_t)enchant_textures[i], ImVec2(24, 24));
-        ImGui::SameLine();
-        ImGui::Checkbox(enchant_names[i], &enchant_enabled[i]);
+static void InitImGui(EGLDisplay display, EGLSurface surface) {
+    eglQuerySurface(display, surface, EGL_WIDTH, &screen_w);
+    eglQuerySurface(display, surface, EGL_HEIGHT, &screen_h);
+
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+
+    ImGuiIO& io = ImGui::GetIO();
+    io.DisplaySize = ImVec2(screen_w, screen_h);
+    io.FontGlobalScale = 1.3f;
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;
+
+    ImGui::StyleColorsDark();
+
+    ImGuiStyle& style = ImGui::GetStyle();
+    style.WindowRounding = 10.0f;
+    style.FrameRounding = 6.0f;
+    style.ScaleAllSizes(1.2f);
+
+    ImGui_ImplAndroid_Init(nullptr);
+    ImGui_ImplOpenGL3_Init("#version 300 es");
+
+    InitTexture();
+
+    imgui_init = true;
+}
+
+static void DrawMenu() {
+    if (!menu_open) return;
+
+    ImGui::SetNextWindowSize(ImVec2(620, 520), ImGuiCond_Once);
+    ImGui::Begin("Enchant Menu", &menu_open, ImGuiWindowFlags_NoCollapse);
+
+    if (ImGui::CollapsingHeader("Enchant", ImGuiTreeNodeFlags_DefaultOpen)) {
+        for (int i = 0; i < 7; i++) {
+            ImGui::PushID(i);
+
+            ImGui::Image((void*)(intptr_t)icon_texture, ImVec2(32, 32));
+            ImGui::SameLine();
+
+            ImGui::Checkbox(enchant_names[i], &enchant_enabled[i]);
+            ImGui::SameLine(360);
+
+            if (ImGui::Button("+", ImVec2(40, 32))) {
+                if (enchant_level[i] < 10)
+                    enchant_level[i]++;
+            }
+
+            ImGui::SameLine();
+            ImGui::Text("Lv %d", enchant_level[i]);
+            ImGui::SameLine();
+
+            if (ImGui::Button("-", ImVec2(40, 32))) {
+                if (enchant_level[i] > 0)
+                    enchant_level[i]--;
+            }
+
+            ImGui::Separator();
+            ImGui::PopID();
+        }
     }
+
     ImGui::End();
 }
 
-static void setup() {
-    if (g_initialized) return;
-    ImGui::CreateContext();
-    ImGui_ImplAndroid_Init();
-    ImGui_ImplOpenGL3_Init("#version 300 es");
-    for (int i = 0; i < 7; i++)
-        enchant_textures[i] = make_texture(epd_bitmap_allArray[i]);
-    g_initialized = true;
-}
+extern "C"
+EGLBoolean eglSwapBuffers(EGLDisplay display, EGLSurface surface) {
+    if (!imgui_init) {
+        InitImGui(display, surface);
+    }
 
-static void render() {
-    glEnable(GL_BLEND);
-    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
     ImGui_ImplOpenGL3_NewFrame();
-    ImGui_ImplAndroid_NewFrame(g_width, g_height);
+    ImGui_ImplAndroid_NewFrame();
     ImGui::NewFrame();
-    draw_menu();
+
+    DrawMenu();
+
     ImGui::Render();
     ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
-}
 
-static EGLBoolean hook_eglswapbuffers(EGLDisplay dpy, EGLSurface surf) {
-    EGLint w, h;
-    eglQuerySurface(dpy, surf, EGL_WIDTH, &w);
-    eglQuerySurface(dpy, surf, EGL_HEIGHT, &h);
-    if (w > 500 && h > 500) {
-        g_width = w;
-        g_height = h;
-        setup();
-        render();
-    }
-    return orig_eglswapbuffers(dpy, surf);
-}
-
-static int32_t (*orig_input)(void*, void*, bool, long, uint32_t*, AInputEvent**) = 0;
-
-static int32_t hook_input(void* thiz, void* a1, bool a2, long a3, uint32_t* a4, AInputEvent** ev) {
-    int32_t r = orig_input(thiz, a1, a2, a3, a4, ev);
-    if (r == 0 && ev && *ev && g_initialized)
-        ImGui_ImplAndroid_HandleInputEvent(*ev);
-    return r;
-}
-
-static void* mainthread(void*) {
-    sleep(3);
-    GlossInit(true);
-    GHandle egl = GlossOpen("libEGL.so");
-    void* swap = (void*)GlossSymbol(egl, "eglSwapBuffers", nullptr);
-    GlossHook(swap, (void*)hook_eglswapbuffers, (void**)&orig_eglswapbuffers);
-
-    GHandle inp = GlossOpen("libinput.so");
-    void* sym = (void*)GlossSymbol(
-        inp,
-        "_ZN7android13InputConsumer7consumeEPNS_26InputEventFactoryInterfaceEblPjPPNS_10InputEventE",
-        nullptr
-    );
-    GlossHook(sym, (void*)hook_input, (void**)&orig_input);
-    return nullptr;
+    return orig_eglSwapBuffers(display, surface);
 }
 
 __attribute__((constructor))
-void init() {
-    pthread_t t;
-    pthread_create(&t, 0, mainthread, 0);
+static void HookEGL() {
+    void* handle = dlopen("libEGL.so", RTLD_LAZY);
+    orig_eglSwapBuffers = (EGLBoolean(*)(EGLDisplay, EGLSurface))
+        dlsym(handle, "eglSwapBuffers");
 }
